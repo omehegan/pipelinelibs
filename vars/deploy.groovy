786d39856d37552fcf0a3918bf5800f8d5dbb63a
Owen Mehegan <omehegan@cloudbees.com>
2018-03-01 21:55:30 +1100

    Print

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,4 +1,4 @@
 def call(String pkgname = '', String version = '', String environment = '') {
-    sh "About to deploy ${pkgname} version ${version} to ${environment}..."
+    print "About to deploy ${pkgname} version ${version} to ${environment}..."
     sh "sleep 5"
 }
