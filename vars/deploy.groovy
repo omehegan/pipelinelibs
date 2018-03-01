823640c80552544f080c9a7f9fc866cbade862d5
Owen Mehegan <omehegan@cloudbees.com>
2018-03-01 21:54:32 +1100

    Refactor

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,4 +1,4 @@
-def deploy(pkgname, version, environment) {
+def call(String pkgname = '', String version = '', String environment = '') {
     sh "About to deploy ${pkgname} version ${version} to ${environment}..."
     sh "sleep 5"
 }
